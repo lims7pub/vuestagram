e001c8e1da56fc6c11a9991d5d56710a9b394088
Liam <lims7design@gmail.com>
2022-11-22 13:27:12 +0900

    Props + style 속성 데이터바인딩

---
+++ src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post :articlePost="articleCont[i]" v-for="(item, i) in articleCont" :key="i" />
+  </div>
+</template>
+
+<script>
+import Post from "./Post";
+
+export default {
+  name: "Container",
+  components: {
+    Post: Post,
+  },
+  props: {
+    articleCont: Array,
+  },
+};
+</script>
+
+<style>
+</style>
